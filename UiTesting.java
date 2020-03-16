9063c9efa449ea21b723ea6938f1060e51805530
Nikish Kumar S V <33373473+nikish007@users.noreply.github.com>
2020-03-16 11:58:21 +0530

    Create UiTesting.java

---
+++ UiTesting.java
@@ -0,0 +1,113 @@
+import java.util.*;
+public class UiTesting{
+public static void main(String[] args){
+              System.out.println("Starting ChromeDriver 79.0.3945.36 (3582db32b33893869b8c1339e8f4d9ed1816f143-refs/branch-heads/3945@{#614}) on port 43103
+Only local connections are allowed.
+Please protect ports used by ChromeDriver and related test frameworks to prevent access by malicious code.
+[1584339498.484][WARNING]: This version of ChromeDriver has not been tested with Chrome version 80.
+[1584339500.500][WARNING]: Timed out connecting to Chrome, retrying...
+Mar 16, 2020 11:48:22 AM org.openqa.selenium.remote.ProtocolHandshake createSession
+INFO: Detected dialect: W3C
+[1584339504.594][WARNING]: Timed out connecting to Chrome, retrying...
+login test case passed
+Starting ChromeDriver 79.0.3945.36 (3582db32b33893869b8c1339e8f4d9ed1816f143-refs/branch-heads/3945@{#614}) on port 24391
+Only local connections are allowed.
+Please protect ports used by ChromeDriver and related test frameworks to prevent access by malicious code.
+[1584339513.987][WARNING]: This version of ChromeDriver has not been tested with Chrome version 80.
+[1584339515.993][WARNING]: Timed out connecting to Chrome, retrying...
+Mar 16, 2020 11:48:38 AM org.openqa.selenium.remote.ProtocolHandshake createSession
+INFO: Detected dialect: W3C
+[1584339520.085][WARNING]: Timed out connecting to Chrome, retrying...
+error detected: passed
+Starting ChromeDriver 79.0.3945.36 (3582db32b33893869b8c1339e8f4d9ed1816f143-refs/branch-heads/3945@{#614}) on port 13057
+Only local connections are allowed.
+Please protect ports used by ChromeDriver and related test frameworks to prevent access by malicious code.
+[1584339528.708][WARNING]: This version of ChromeDriver has not been tested with Chrome version 80.
+[1584339530.713][WARNING]: Timed out connecting to Chrome, retrying...
+Mar 16, 2020 11:48:52 AM org.openqa.selenium.remote.ProtocolHandshake createSession
+INFO: Detected dialect: W3C
+[1584339534.803][WARNING]: Timed out connecting to Chrome, retrying...
+forgot_password passed
+Starting ChromeDriver 79.0.3945.36 (3582db32b33893869b8c1339e8f4d9ed1816f143-refs/branch-heads/3945@{#614}) on port 17459
+Only local connections are allowed.
+Please protect ports used by ChromeDriver and related test frameworks to prevent access by malicious code.
+[1584339548.666][WARNING]: This version of ChromeDriver has not been tested with Chrome version 80.
+[1584339550.681][WARNING]: Timed out connecting to Chrome, retrying...
+Mar 16, 2020 11:49:12 AM org.openqa.selenium.remote.ProtocolHandshake createSession
+INFO: Detected dialect: W3C
+[1584339554.763][WARNING]: Timed out connecting to Chrome, retrying...
+error detected:passed
+Starting ChromeDriver 79.0.3945.36 (3582db32b33893869b8c1339e8f4d9ed1816f143-refs/branch-heads/3945@{#614}) on port 35050
+Only local connections are allowed.
+Please protect ports used by ChromeDriver and related test frameworks to prevent access by malicious code.
+[1584339569.018][WARNING]: This version of ChromeDriver has not been tested with Chrome version 80.
+[1584339571.024][WARNING]: Timed out connecting to Chrome, retrying...
+Mar 16, 2020 11:49:33 AM org.openqa.selenium.remote.ProtocolHandshake createSession
+INFO: Detected dialect: W3C
+[1584339575.110][WARNING]: Timed out connecting to Chrome, retrying...
+readmore testcase passed
+Starting ChromeDriver 79.0.3945.36 (3582db32b33893869b8c1339e8f4d9ed1816f143-refs/branch-heads/3945@{#614}) on port 31377
+Only local connections are allowed.
+Please protect ports used by ChromeDriver and related test frameworks to prevent access by malicious code.
+[1584339585.024][WARNING]: This version of ChromeDriver has not been tested with Chrome version 80.
+[1584339587.041][WARNING]: Timed out connecting to Chrome, retrying...
+Mar 16, 2020 11:49:49 AM org.openqa.selenium.remote.ProtocolHandshake createSession
+INFO: Detected dialect: W3C
+[1584339591.097][WARNING]: Timed out connecting to Chrome, retrying...
+submit_guest_details test case passed
+Starting ChromeDriver 79.0.3945.36 (3582db32b33893869b8c1339e8f4d9ed1816f143-refs/branch-heads/3945@{#614}) on port 2933
+Only local connections are allowed.
+Please protect ports used by ChromeDriver and related test frameworks to prevent access by malicious code.
+[1584339609.860][WARNING]: This version of ChromeDriver has not been tested with Chrome version 80.
+[1584339611.865][WARNING]: Timed out connecting to Chrome, retrying...
+Mar 16, 2020 11:50:13 AM org.openqa.selenium.remote.ProtocolHandshake createSession
+INFO: Detected dialect: W3C
+[1584339615.950][WARNING]: Timed out connecting to Chrome, retrying...
+rest_for test case passed
+Starting ChromeDriver 79.0.3945.36 (3582db32b33893869b8c1339e8f4d9ed1816f143-refs/branch-heads/3945@{#614}) on port 24308
+Only local connections are allowed.
+Please protect ports used by ChromeDriver and related test frameworks to prevent access by malicious code.
+[1584339634.966][WARNING]: This version of ChromeDriver has not been tested with Chrome version 80.
+[1584339636.971][WARNING]: Timed out connecting to Chrome, retrying...
+Mar 16, 2020 11:50:39 AM org.openqa.selenium.remote.ProtocolHandshake createSession
+INFO: Detected dialect: W3C
+[1584339641.068][WARNING]: Timed out connecting to Chrome, retrying...
+student_details test case passed
+Starting ChromeDriver 79.0.3945.36 (3582db32b33893869b8c1339e8f4d9ed1816f143-refs/branch-heads/3945@{#614}) on port 29530
+Only local connections are allowed.
+Please protect ports used by ChromeDriver and related test frameworks to prevent access by malicious code.
+[1584339656.432][WARNING]: This version of ChromeDriver has not been tested with Chrome version 80.
+[1584339658.439][WARNING]: Timed out connecting to Chrome, retrying...
+Mar 16, 2020 11:51:00 AM org.openqa.selenium.remote.ProtocolHandshake createSession
+INFO: Detected dialect: W3C
+[1584339662.493][WARNING]: Timed out connecting to Chrome, retrying...
+students_back test case passed
+Starting ChromeDriver 79.0.3945.36 (3582db32b33893869b8c1339e8f4d9ed1816f143-refs/branch-heads/3945@{#614}) on port 43994
+Only local connections are allowed.
+Please protect ports used by ChromeDriver and related test frameworks to prevent access by malicious code.
+[1584339679.927][WARNING]: This version of ChromeDriver has not been tested with Chrome version 80.
+[1584339681.931][WARNING]: Timed out connecting to Chrome, retrying...
+Mar 16, 2020 11:51:23 AM org.openqa.selenium.remote.ProtocolHandshake createSession
+INFO: Detected dialect: W3C
+[1584339685.982][WARNING]: Timed out connecting to Chrome, retrying...
+logout_main case passed
+Starting ChromeDriver 79.0.3945.36 (3582db32b33893869b8c1339e8f4d9ed1816f143-refs/branch-heads/3945@{#614}) on port 22398
+Only local connections are allowed.
+Please protect ports used by ChromeDriver and related test frameworks to prevent access by malicious code.
+[1584339702.001][WARNING]: This version of ChromeDriver has not been tested with Chrome version 80.
+[1584339704.006][WARNING]: Timed out connecting to Chrome, retrying...
+Mar 16, 2020 11:51:46 AM org.openqa.selenium.remote.ProtocolHandshake createSession
+INFO: Detected dialect: W3C
+[1584339708.057][WARNING]: Timed out connecting to Chrome, retrying...
+logout_students_details test case passed
+Starting ChromeDriver 79.0.3945.36 (3582db32b33893869b8c1339e8f4d9ed1816f143-refs/branch-heads/3945@{#614}) on port 20487
+Only local connections are allowed.
+Please protect ports used by ChromeDriver and related test frameworks to prevent access by malicious code.
+[1584339726.389][WARNING]: This version of ChromeDriver has not been tested with Chrome version 80.
+[1584339728.393][WARNING]: Timed out connecting to Chrome, retrying...
+Mar 16, 2020 11:52:10 AM org.openqa.selenium.remote.ProtocolHandshake createSession
+INFO: Detected dialect: W3C
+[1584339732.447][WARNING]: Timed out connecting to Chrome, retrying...
+error detected: passed");
+}
+}
